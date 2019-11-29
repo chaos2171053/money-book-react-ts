fc815df8ed04fde5550569b341506a5d533754a1
chaos2171053 <chaos2171053@gmail.com>
2019-11-29 17:09:02 +0800

    CategorySelect css

--- src/components/CategorySelect.tsx
+++ src/components/CategorySelect.tsx
@@ -1,8 +1,8 @@
 import React, { Component } from 'react'
 import Ionicon from 'react-ionicons'
-
+import { Colors } from '../utility'
 interface Category {
-    id: number,
+    id: string,
     name: string,
     type: string,
     iconName: string
@@ -10,12 +10,12 @@ interface Category {
 
 interface IProps {
     categories: Array<Category>,
-    selectedCategory: Category
+    selectedCategory?: Category
     onSelectCategory: Function
 }
 
 interface IState {
-    selectedCategoryId?: number
+    selectedCategoryId?: string | null
 }
 
 
@@ -24,7 +24,7 @@ class CategorySelect extends Component<IProps, IState> {
     constructor(props: IProps) {
         super(props)
         this.state = {
-            selectedCategoryId: props.selectedCategory && props.selectedCategory.id
+            selectedCategoryId: props.selectedCategory ? props.selectedCategory.id : null
         }
     }
     selectCatory = (event: any, category: Category) => {
@@ -41,10 +41,19 @@ class CategorySelect extends Component<IProps, IState> {
             <div className="category-select-component">
                 <div className="row">
                     {categories.map((category, index) => {
+                        const iconColor = (category.id === selectedCategoryId) ? Colors.white : Colors.gray
+                        const backColor = (category.id === selectedCategoryId) ? Colors.blue : Colors.lightGray
                         const activeClassName = (selectedCategoryId && (selectedCategoryId === category.id)) ? 'category-item col-3 active' : 'category-item col-3'
                         return (
                             <div className={activeClassName} key={index} onClick={(event) => this.selectCatory(event, category)}>
-                                <Ionicon clasName="rounded-circle" fontSize="50px" color="#555" icon={category.iconName}></Ionicon>
+                                <Ionicon
+                                    className="rounded-circle"
+                                    style={{ backgroundColor: backColor, padding: '5px' }}
+                                    fontSize="50px"
+                                    color={iconColor}
+                                    icon={category.iconName}>
+
+                                </Ionicon>
                             </div>
                         )
                     })}
--- src/containers/Create.tsx
+++ src/containers/Create.tsx
@@ -1,16 +1,29 @@
-import React from 'react'
+import React, { Component } from 'react'
 import PriceForm from '../components/PriceForm'
-import { testItems } from '../testData'
-
-const Create = ({
-    match
-}: { [key: string]: any } = {}) => {
-    return (
-        <div>
-            this is a new page{match.params.id}
-            <PriceForm onFormSubmit={() => { }} onCancelSubmit={() => { }} item={testItems[0]}></PriceForm>
-        </div >
-    )
+import { testItems, testCategories } from '../testData'
+import { Tabs, Tab } from '../components/Tabs'
+import { TYPE_INCOME, TYPE_OUTCOME } from '../utility'
+import CategorySelect from '../components/CategorySelect'
+interface Ipros {
+    match: { [key: string]: any }
 }
 
-export default Create
+export default class Create extends Component<Ipros>{
+    render() {
+        const filterCategories = testCategories.filter(category => {
+            return category.type === TYPE_OUTCOME
+        })
+        return (
+            <div className="create-page py-3 px-3 rounded mt-3" style={{ background: '#fff' }}>
+                <Tabs activeIndex={0} onTabChange={() => { }}>
+                    <Tab>支出</Tab>
+                    <Tab>收入</Tab>
+                </Tabs>
+                <CategorySelect categories={filterCategories}
+                    onSelectCategory={() => { }}
+                />
+                <PriceForm onFormSubmit={() => { }} onCancelSubmit={() => { }} ></PriceForm>
+            </div >
+        )
+    }
+}
