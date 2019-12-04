954694cb97930f43ca38be631e0b38e2335208a6
chaos2171053 <chaos2171053@gmail.com>
2019-12-04 16:36:45 +0800

    add proxy axios

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,20 @@ import { App } from './App';
 import * as serviceWorker from './serviceWorker';
 import axios from 'axios'
 
+axios.get('http://localhost:3004/items/_dotug7vnn11222')
+    .then(function (response) {
+        // handle success
+        console.log(response);
+    })
+    .catch(function (error) {
+        // handle error
+        console.log(error);
+    })
+    .finally(function () {
+        // always executed
+    });
+
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
